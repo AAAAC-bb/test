4bd69345f1be421ae04f58b63103c25b30aa4a76
AAAAC-bb <charlanders1@gmail.com>
2017-08-13 04:34:07 +0800

    run script

--- shell.py
+++ shell.py
@@ -1,4 +1,3 @@
-from audit.backend import user_interactive
 import sys
 import os
 import django
@@ -7,5 +6,7 @@ import django
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blj.settings")
     django.setup()
+    from audit.backend import user_interactive
+
     obj = user_interactive.UserShell(sys.argv)
     obj.start()
