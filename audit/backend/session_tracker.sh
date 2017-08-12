44b97d1b5d8f5eef0a6ece7d7cc91277f2eb5857
AAAAC-bb <charlanders1@gmail.com>
2017-08-13 03:56:11 +0800

    bash

--- audit/backend/session_tracker.sh
+++ audit/backend/session_tracker.sh
@@ -2,7 +2,7 @@
 
 for i in $(seq 1 30); do
     random_str=$1
-    process_id = `ps -ef | grep $1 | egrep -v 'grep|sshpass|session_tracker' | awk '{print $2}'`
+    process_id=`ps -ef | grep $1 | egrep -v 'grep|sshpass|session_tracker' | awk '{print $2}'`
     echo $process_id
     if [ ! -z "$process_id" ];then
         echo "start run strace..."
